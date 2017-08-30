a3d7fc19d0e2c6964c2e5476c073ca6ed86f5d72
Antonio Tsai <jimmyjack7777@gmail.com>
2017-08-30 12:56:52 +0800

    Add about

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="containner">
+    <div>
+      <h1>
+        About Me
+      </h1>
+      <p>
+        大同資工畢業，主要技能為前端工程師，喜愛網頁開發，對於與視覺化有關的技術感興趣，如VR、Web、 Data Visualization。最近對於大數據有點興趣，受 COSCUP 演講啟發而開始打 Kaggle 中。
+      </p>
+      <p>
+        希望能貫徹「能排程處理、自動化的事，就不要手動處理」的精神，興趣利用自己所擁有的技術去開發使生活更便利的小工具。
+      </p>
+    </div>
+  </section>
+</template>
