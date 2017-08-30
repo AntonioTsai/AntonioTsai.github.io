a84f887b9849d11e8e5de75ab59c890ba8bf5fbb
Antonio Tsai <jimmyjack7777@gmail.com>
2017-08-30 17:37:50 +0800

    Add projects

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,5 +1,21 @@
 <template>
   <section>
-    
+    <h1>PROJECTS</h1>
+    <div>
+      <h2>Girl Frontline Calculator</h2>
+      #Vue.js #Semantic-UI
+    </div>
+    <div>
+      <h2>SF Comic Viewer</h2>
+      #jQuery
+    </div>
+    <div>
+      <h2>TDOH Skilltree</h2>
+      #D3.js #Semantic-UI
+    </div>
+    <div>
+      <h2>TTU-Course</h2>
+      #Materialize
+    </div>
   </section>
 </template>
