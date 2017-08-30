d2895c62ae52963db624ee8536b870d8f657293d
Antonio Tsai <jimmyjack7777@gmail.com>
2017-08-30 16:15:42 +0800

    Add content of experience

---
+++ pages/experience.vue
@@ -0,0 +1,41 @@
+<template>
+  <section>
+    <div>
+      <h1>EXPERIENCES</h1>
+      <h2>JM-PLUS (2016)</h2>
+      <h3>
+        Front-End Developer
+      </h3>
+      <ul>
+        <li>
+          Developed custom in-ear monitor website used on iPad using jQuery and Materialize
+        </li>
+        <li>
+          Improved UI and User Experience based on customer and salespeople's feedback.
+        </li>
+        <li>
+          Used JavaScript and AJAX for client-side rendering.
+        </li>
+        <li>
+          Manage development flow using Git and Git-flow.
+        </li>
+        <li>
+          Use Gulp and Browsersync for Testing.
+        </li>
+      </ul>
+
+      <h2>inwinSTACK (Jul. 2015 - Aug. 2015)</h2>
+      <h3>
+        Intern
+      </h3>
+      <ul>
+        <li>
+          Surveyed the basic architecture and deploy Openstack.
+        </li>
+        <li>
+          Contributed to Openstack by fixing bugs.
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
