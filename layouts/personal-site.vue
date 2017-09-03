fb6e006e67850e3d16e512da38e489a516577bef
Antonio Tsai <jimmyjack7777@gmail.com>
2017-09-03 17:42:56 +0800

    Add nav to layout

--- layouts/personal-site.vue
+++ layouts/personal-site.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="container">
+    <div class="header">
+      <b-nav tabs class="float-right">
+        <b-nav-item active>About Me</b-nav-item>
+        <b-nav-item>Skills</b-nav-item>
+        <b-nav-item>Experiences</b-nav-item>
+        <b-nav-item>Projects</b-nav-item>
+        <b-nav-item>Contact</b-nav-item>
+      </b-nav>
+      <h2 class="text-muted">Fung-Nien Tsai | Antonio</h2>
+    </div>
     <nuxt/>
   </div>
 </template>
