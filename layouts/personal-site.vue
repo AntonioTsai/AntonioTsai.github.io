76002e20c9cf8cf5978c1f50fe4ed3a4e5372237
Antonio Tsai <jimmyjack7777@gmail.com>
2017-09-03 22:55:33 +0800

    Center h2 horizontally

--- layouts/personal-site.vue
+++ layouts/personal-site.vue
@@ -1,6 +1,6 @@
 <template>
   <b-container>
-    <div class="header">
+    <div class="header clearfix">
       <b-nav tabs class="float-right">
         <b-nav-item active>About Me</b-nav-item>
         <b-nav-item>Skills</b-nav-item>
@@ -8,9 +8,9 @@
         <b-nav-item>Projects</b-nav-item>
         <b-nav-item>Contact</b-nav-item>
       </b-nav>
-      <div class="clearfix row">
-        <b-img rounded="circle" width="60" height="60" class="float-left" src="https://www.gravatar.com/avatar/28150f77152f009b564a81c0a0b603e1" alt="Gravatar"></b-img>
-        <h2 class="text-muted my-auto">蔡豐年 | Fung-Nien Tsai | Antonio</h2>
+      <div class="brand">
+        <b-img left rounded="circle" width="60" height="60" src="https://www.gravatar.com/avatar/28150f77152f009b564a81c0a0b603e1" alt="Gravatar"></b-img>
+        <h2 class="text-muted align-middle">蔡豐年 | Fung-Nien Tsai | Antonio</h2>
       </div>
     </div>
     <nuxt/>
@@ -18,6 +18,10 @@
 </template>
 
 <style>
+.brand h2 {
+  line-height: 60px;
+}
+
 body {
   padding-top: 2.5rem;
 }
