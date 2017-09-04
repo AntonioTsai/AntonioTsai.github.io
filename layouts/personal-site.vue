c46fed71725257909e72d4a6d76cc472df3600a7
Antonio Tsai <jimmyjack7777@gmail.com>
2017-09-04 18:39:18 +0800

    Add nav for small screen

--- layouts/personal-site.vue
+++ layouts/personal-site.vue
@@ -7,6 +7,14 @@
         <b-nav-item href="experiences">Experiences</b-nav-item>
         <b-nav-item href="projects">Projects</b-nav-item>
       </b-nav>
+      <b-nav class="float-right d-lg-none">
+        <b-nav-item-dropdown id="icon_menu">
+          <b-dropdown-item>About Me</b-dropdown-item>
+          <b-dropdown-item>Skills</b-dropdown-item>
+          <b-dropdown-item>Experiences</b-dropdown-item>
+          <b-dropdown-item>Projects</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-nav>
       <div class="brand">
         <b-img left rounded="circle" src="https://www.gravatar.com/avatar/28150f77152f009b564a81c0a0b603e1" alt="Gravatar"></b-img>
         <h3 class="text-muted align-middle">蔡豐年 | Fung-Nien Tsai | Antonio</h3>
