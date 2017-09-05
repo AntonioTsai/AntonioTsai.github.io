318cadc5d1b733bfae31fad662cc699d50b91a14
Antonio Tsai <jimmyjack7777@gmail.com>
2017-09-05 16:55:23 +0800

    Use collapse navbar & finish lg screen size(>=992)

--- layouts/personal-site.vue
+++ layouts/personal-site.vue
@@ -28,6 +28,30 @@
       </div>
     </div>
 
+    <b-navbar toggleable="md" sticky>
+
+      <b-nav-toggle target="nav_collapse"></b-nav-toggle>
+
+      <b-navbar-brand href="#" class="brand">
+        <b-img left rounded="circle" src="https://www.gravatar.com/avatar/28150f77152f009b564a81c0a0b603e1" alt="Gravatar"></b-img>
+        <h3 class="text-muted align-middle">蔡豐年 | Fung-Nien Tsai | Antonio</h3>
+      </b-navbar-brand>
+
+      <b-collapse is-nav id="nav_collapse">
+
+        <!-- Right aligned nav items -->
+        <b-nav is-nav-bar class="ml-auto">
+          <!-- nav for screen size >= lg(992px) -->
+          <b-nav tabs class="d-none d-lg-flex">
+            <b-nav-item href="/about" active>About Me</b-nav-item>
+            <b-nav-item href="/skills">Skills</b-nav-item>
+            <b-nav-item href="experiences">Experiences</b-nav-item>
+            <b-nav-item href="projects">Projects</b-nav-item>
+          </b-nav>
+        </b-nav>
+      </b-collapse>
+    </b-navbar>
+
     <nuxt/>
   </b-container>
 </template>
