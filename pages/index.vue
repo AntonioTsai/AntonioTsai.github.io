ed47d729537dec95265d7d021f5e5036bbca9033
Antonio Tsai <jimmyjack7777@gmail.com>
2017-09-10 01:16:13 +0800

    Move period out of the del tag

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <b-jumbotron class="text-center">
       <h1 slot="header" class="display-4">About Me</h1>
       <p slot="lead" class="mx-5 my-2">喜歡研究各式各樣的網頁開發技術，對視覺化相關的技術很感興趣，只要是關於網站開發的技術都樂於學習。偶爾會設計些小工具讓生活可以過的更便利點，能自動化的事，就不要手動處理，畢竟，科技始終於人性，
-        <del>人性就是懶惰。</del>
+        <del>人性就是懶惰</del>。
       </p>
       <hr class="mx-5 my-4">
       <b-row class="justify-content-lg-center">
