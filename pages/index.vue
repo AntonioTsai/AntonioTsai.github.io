746588c6ce102de44d094f1d09091ca2523501b3
Antonio Tsai <jimmyjack7777@gmail.com>
2017-09-11 21:41:44 +0800

    Show resume & contact info horizontally

--- pages/index.vue
+++ pages/index.vue
@@ -5,28 +5,36 @@
       <template slot="lead" class="mx-5 my-2">喜歡研究各式各樣的網頁開發技術，對視覺化相關的技術很感興趣，只要是關於網站開發的技術都樂於學習。偶爾會設計些小工具讓生活可以過的更便利點，能自動化的事，就不要手動處理，畢竟，科技始終於人性，
         <del>人性就是懶惰</del>。
       </template>
+
       <hr class="mx-5 my-4">
-      <h2>Resume</h2>
-      <b-row class="justify-content-center">
-        <b-col cols="auto">
-          <b-button variant="primary" size="sm" :href="resume.href" class="resume">{{ resume.text}}
-            <i class="material-icons align-middle">{{ resume.icon }}</i>
-          </b-button>
+      <b-row>
+        <b-col cols="12" lg="6">
+          <h2>Contact</h2>
+          <b-row class="justify-content-center">
+            <b-col cols="12">
+              <template v-for="button in contacts">
+                <address :key="`_${button.name}`">
+                  <strong>{{ button.name + ': ' }}</strong>
+                  <a :href="button.href">{{ button.text }}</a>
+                </address>
+              </template>
+            </b-col>
+          </b-row>
+        </b-col>
+        <b-col md="12" class="d-block d-lg-none">
+          <hr class="mx-5 my-4">
+        </b-col>
+        <b-col cols="12" lg="6">
+          <h2>Resume</h2>
+          <b-row class="justify-content-center">
+            <b-col cols="auto">
+              <b-button variant="primary" size="sm" :href="resume.href" class="resume">{{ resume.text}}
+                <i class="material-icons align-middle">{{ resume.icon }}</i>
+              </b-button>
+            </b-col>
+          </b-row>
         </b-col>
       </b-row>
-      <hr class="mx-5 my-4">
-      <h2>Contact</h2>
-      <b-row class="justify-content-center">
-        <template v-for="button in contacts">
-          <b-col cols="12" :key="`_${button.name}`">
-            <address>
-              <strong>{{ button.name + ': ' }}</strong>
-              <a :href="button.href">{{ button.text }}</a>
-            </address>
-          </b-col>
-        </template>
-      </b-row>
-
     </b-jumbotron>
   </section>
 </template>
